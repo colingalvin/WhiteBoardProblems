db03581c0a1185f0223213f5240c9589e3e1ed5e
colingalvin <colingalvin@ymail.com>
2021-01-07 14:06:15 -0500

    Compres string - Java.

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // FizzBuzz();
         // ReverseString();
-        CapitalizeFirstLetters();
+        // CapitalizeFirstLetters();
+        CompressString();
     }
 
     public static void FizzBuzz() {
@@ -46,5 +47,31 @@ public class Main {
             }
         }
         System.out.print("Your capitalized input is " + capitalizedUserInput);
+    }
+
+    public static void CompressString() {
+        System.out.print("Enter a string of characters to be compressed: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        StringBuilder compressedUserInput = new StringBuilder();
+        int counter = 1;
+        for (int i = 0; i < userInput.length(); i++) {
+            if (i == 0) {
+                if (userInput.charAt(i) != userInput.charAt(i + 1)) {
+                    compressedUserInput.append(userInput.charAt(i));
+                }
+            } else if (userInput.charAt(i) == userInput.charAt(i - 1)) {
+                counter++;
+            } else if (i > 1) {
+                compressedUserInput.append(counter);
+                compressedUserInput.append(userInput.charAt(i - 1));
+                counter = 1;
+            }
+            if (i == userInput.length() - 1) {
+                compressedUserInput.append(counter);
+                compressedUserInput.append(userInput.charAt(i));
+            }
+        }
+        System.out.println("Your compressed input is: " + compressedUserInput);
     }
 }
